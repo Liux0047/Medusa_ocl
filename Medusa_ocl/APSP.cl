d37bcb510ac198af0fc61bbbfe245445d2b0eb9c
Allen <tracy1kobe@gmail.com>
2015-03-04 22:44:12 +0800

    APSP

---
+++ Medusa_ocl/APSP.cl
@@ -0,0 +1,101 @@
+// Copyright (c) 2009-2013 Intel Corporation
+// All rights reserved.
+//
+// WARRANTY DISCLAIMER
+//
+// THESE MATERIALS ARE PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL INTEL OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THESE
+// MATERIALS, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Intel Corporation is the author of the Materials, and requests that all
+// problem reports or change requests be submitted to it directly
+
+
+/*  The following macros should be defined in the build options:
+
+    - T -- type of matrix elements and all calculations
+
+    Tiles sizes and group sizes:
+
+    - TILE_SIZE_M -- the number of elements of A processed in one WI
+    - TILE_GROUP_M -- group/tile size along matrix A (NDRange dimension 0)
+    - TILE_SIZE_N -- the number of elements of B processed in one WI
+    - TILE_GROUP_N -- group/tile size along matrix B (NDRange dimension 1)
+    - TILE_SIZE_K -- size of a tile along dot-product dimension
+*/
+
+#ifdef SAMPLE_NEEDS_DOUBLE
+    #pragma OPENCL EXTENSION cl_khr_fp64: enable
+#endif
+
+
+// sendMsg function
+// edge_count_list is the list of edge counts in VertexArray
+// rank_list is the list of vertex ranks in VertexArray
+// edge_msg_list is the pointer to a list of edge messages
+// edge_offset_list is the pointer to a list of edge offsets as in CAA layout
+__kernel void send_msg (
+	global T *rank_list,
+	global int *edge_count,
+	global int *edge_offset_list,
+	global T *edge_msg_list
+)
+{
+	int id = get_global_id(0);
+		
+	T msg = (rank_list[id] + edge_count[id]/2) / edge_count[id];	//round to nearest integer
+
+	//broadcast to all out edges
+	//edge list stored in CAA format	
+	int edge_id = id;
+	
+	while (edge_offset_list[edge_id] != -1) { 
+		edge_msg_list[edge_id] = msg;
+		edge_id += edge_offset_list[edge_id];
+	} 	
+	edge_msg_list[edge_id] = msg;
+	
+	
+}
+
+
+/*
+ * combine messages from edges
+ */
+__kernel void combine (
+	global int *tail_vertex,
+	global T *edge_msg_list,
+	global int *edge_offset_list,
+	global T *rank_list_output
+)
+{	
+
+
+	int id = get_global_id(0);
+	
+	int edge_id = id;
+	int offset = edge_offset_list[id];
+	rank_list_output[id] = 0;
+
+	 while (edge_offset_list[edge_id] != -1) { 
+
+		// atomic add floats
+		//while ( atom_cmpxchg( mutex[ tail_vertex[edge_id] ], 0 ,1 ) );
+		atomic_add(&rank_list_output[ tail_vertex[edge_id] ], edge_msg_list[edge_id]);
+		//atom_xchg ( mutex[ tail_vertex[edge_id] ], 0 );
+		
+		edge_id += edge_offset_list[edge_id];
+	};
+	atomic_add(&rank_list_output[ tail_vertex[edge_id] ], edge_msg_list[edge_id]);
+	
+
+	
+}
---
+++ Medusa_ocl/APSP.hpp
@@ -0,0 +1,380 @@
+
+#ifndef _APSP_HPP_
+#define _APSP_HPP_
+
+//Medusa data structures
+#include "vertex.hpp"
+#include "edge.hpp"
+#include "cmdoptions.hpp"
+
+//construct the data from the file
+template <typename T>
+void constructData(
+	int vertexCount,
+	int edgeCount,
+	VertexArray<T> &vertexArray,
+	EdgeArray<T> &edgeArray
+	) {
+
+	ifstream inDataFile("data/small-sample-APSP.txt", ios::in);
+
+	if (!inDataFile) {
+		cerr << "File could not be opened" << endl;
+		exit(1);
+	}
+
+	edgeCount = vertexCount * vertexCount;
+	
+	//dynamic allocate a 2D array
+	int** edge = new int*[vertexCount];
+	for (int i = 0; i < vertexCount; ++i) {
+		edge[i] = new int[vertexCount];
+	}
+
+	int edgeWeight;
+	//read into the edge [][] array 
+	int rowNum = 0;		//row number is the head vertex
+	int colNum = 0;		//col number is the tail vertex
+	while (inDataFile >> edgeWeight && rowNum < vertexCount) {	//read till EOF and close the file
+		edge[rowNum][colNum] = edgeWeight;
+		colNum++;
+		if (colNum % vertexCount == 0) {	//next line
+			colNum = 0;
+			rowNum++;
+		}
+	}
+
+	//initialize edges
+	int *tail_vertex = new int[static_cast<int> (edgeCount)];
+	int *offset = new int[static_cast<int> (edgeCount)];
+	T *message = new T[static_cast<int> (edgeCount)];
+	int *vertex_edge_count = new int[static_cast<int> (vertexCount)];
+
+	for (size_t i = 0; i < edgeCount; i++) {
+		tail_vertex[i] = 0;
+		offset[i] = 0;
+	}
+
+	for (size_t i = 0; i < vertexCount; i++) {
+		vertex_edge_count[i] = 0;
+	}
+
+	edgeArray.tail_vertex = tail_vertex;
+	edgeArray.offset = offset;
+	edgeArray.message = message;
+	vertexArray.edge_count = vertex_edge_count;
+
+
+	int *lastEdgePos = new int[static_cast<int> (vertexCount)];	// the position of last out edge for each vertex
+	int *currentCol = new int[static_cast<int> (vertexCount)];	// the position of current col number for each vertex in the input matrix
+	//initialize last edge position array
+	for (size_t i = 0; i < vertexCount; i++) {
+		lastEdgePos[i] = LAST_OUT_EDGE;
+		currentCol[i] = 0;
+	}
+
+	// construct CAA in column major foramt
+	int numEdgesAdded = 0;
+	bool halt = false;
+	while (!halt) {
+		halt = true;
+		for (int row = 0; row < vertexCount; row++){
+			
+			if (currentCol[row] < (static_cast<int> (vertexCount))) {
+				halt = false;
+				vertexArray.edge_count[row]++;
+				//record the tail vertex
+				(edgeArray.tail_vertex)[numEdgesAdded] = currentCol[row];
+				//record the offset
+				if (lastEdgePos[row] != LAST_OUT_EDGE) {
+					edgeArray.offset[lastEdgePos[row]] = numEdgesAdded - lastEdgePos[row];
+				}
+				lastEdgePos[row] = numEdgesAdded;
+				numEdgesAdded++;
+			}
+			currentCol[row]++;
+		}
+	}
+
+	for (int i = 0; i < vertexCount; i++){
+		edgeArray.offset[lastEdgePos[i]] = LAST_OUT_EDGE;
+	}
+
+	//cleaning up
+	delete[] lastEdgePos;
+	for (int i = 0; i < vertexCount; ++i) {
+		delete[] edge[i];
+	}
+	delete[] edge;
+
+	//output for test
+	cout << "Tail vertex:" << endl;
+	for (int i = 0; i < edgeCount; i++) {
+		cout << edgeArray.tail_vertex[i] << " ";
+	}
+	cout << endl;
+	cout << "number of edges on each vertex" << endl;
+	for (int i = 0; i < vertexCount; i++) {
+		cout << vertexArray.edge_count[i] << " ";
+	}
+	cout << endl;
+	cout << "Offset:" << endl;
+	for (int i = 0; i < edgeCount; i++) {
+		cout << edgeArray.offset[i] << " ";
+	}
+	cout << endl;
+
+}
+
+
+// OpenCL host side code.
+template <typename T>
+void medusa(
+	CmdParserMedusa& cmdparser,
+	OpenCLBasic& oclobjects,
+	OpenCLProgramOneKernel& sendMsgKernel,
+	OpenCLProgramOneKernel& combineKernel,
+	size_t vertex_count,
+	size_t edge_count,
+	VertexArray<T> &vertexArray,
+	EdgeArray<T> &edgeArray
+	)
+{
+	// -----------------------------------------------------------------------
+	// Calculating, allocating and initializing host-side memory
+	// -----------------------------------------------------------------------
+
+	cout
+		<< "Running Medusa"
+		<< " kernel with vertex count: " << vertex_count << "\n";
+
+
+	size_t vertex_rank_memory_size = vertex_count * sizeof(T);
+	size_t vertex_edge_count_memory_size = vertex_count * sizeof(int);
+	size_t edge_msg_memory_size = edge_count * sizeof(T);
+	size_t edge_offset_memory_size = edge_count * sizeof(int);
+	size_t tail_vertex_memory_size = edge_count * sizeof(int);
+	cout << "Size of memory region for vertex rank: " << vertex_rank_memory_size << " bytes\n";
+	cout << "Size of memory region for edge message: " << edge_msg_memory_size << " bytes\n";
+	cout << "Size of memory region for edge offset: " << edge_offset_memory_size << " bytes\n";
+
+	// Allocate aligned memory for vertex ranks to use them in
+	// buffers with CL_MEM_USE_HOST_PTR.
+	// a pair of pointer and cl_mem object; cl_mem object is
+	// be creater later.
+
+
+
+	// -----------------------------------------------------------------------
+	// Allocating device-side resources
+	// -----------------------------------------------------------------------
+	cout << "Allocating device-side resources \n";
+
+	cl_int err = 0; // OpenCL error code
+
+	// Create OpenCL buffers for the matrices based on allocated memory regions
+	// Create buffers with CL_MEM_USE_HOST_PTR to minimize copying and
+	// model situation when matrices are hosted by some native library that
+	// uses OpenCL to accelerate calculations.
+
+	cl_mem vertex_rank_buffer = clCreateBuffer(
+		oclobjects.context,
+		CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
+		vertex_rank_memory_size,
+		vertexArray.vertex_rank,
+		&err
+		);
+	SAMPLE_CHECK_ERRORS(err);
+
+	cl_mem vertex_edge_count_buffer = clCreateBuffer(
+		oclobjects.context,
+		CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+		vertex_edge_count_memory_size,
+		vertexArray.edge_count,
+		&err
+		);
+	SAMPLE_CHECK_ERRORS(err);
+
+	cl_mem edge_msg_buffer = clCreateBuffer(
+		oclobjects.context,
+		CL_MEM_WRITE_ONLY,
+		edge_msg_memory_size,
+		NULL,
+		&err
+		);
+	SAMPLE_CHECK_ERRORS(err);
+
+	cl_mem edge_offset_buffer = clCreateBuffer(
+		oclobjects.context,
+		CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+		edge_offset_memory_size,
+		edgeArray.offset,
+		&err
+		);
+	SAMPLE_CHECK_ERRORS(err);
+
+	cl_mem tail_vertex_buffer = clCreateBuffer(
+		oclobjects.context,
+		CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+		tail_vertex_memory_size,
+		edgeArray.tail_vertex,
+		&err
+		);
+	SAMPLE_CHECK_ERRORS(err);
+
+	cl_mem vertex_rank_output_buffer = clCreateBuffer(
+		oclobjects.context,
+		CL_MEM_READ_WRITE,
+		vertex_rank_memory_size,
+		NULL,
+		&err
+		);
+	SAMPLE_CHECK_ERRORS(err);
+
+
+
+	// -----------------------------------------------------------------------
+	// Setting kernel arguments for sendMsg
+	// -----------------------------------------------------------------------
+
+	err = clSetKernelArg(sendMsgKernel.kernel, 0, sizeof(cl_mem), &vertex_rank_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+	err = clSetKernelArg(sendMsgKernel.kernel, 1, sizeof(cl_mem), &vertex_edge_count_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+	err = clSetKernelArg(sendMsgKernel.kernel, 2, sizeof(cl_mem), &edge_offset_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+	err = clSetKernelArg(sendMsgKernel.kernel, 3, sizeof(cl_mem), &edge_msg_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+
+
+	// -----------------------------------------------------------------------
+	// Setting kernel arguments for combine
+	// -----------------------------------------------------------------------
+
+	err = clSetKernelArg(combineKernel.kernel, 0, sizeof(cl_mem), &tail_vertex_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+	err = clSetKernelArg(combineKernel.kernel, 1, sizeof(cl_mem), &edge_msg_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+	err = clSetKernelArg(combineKernel.kernel, 2, sizeof(cl_mem), &edge_offset_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+	err = clSetKernelArg(combineKernel.kernel, 3, sizeof(cl_mem), &vertex_rank_output_buffer);
+	SAMPLE_CHECK_ERRORS(err);
+
+	// -----------------------------------------------------------------------
+	// Define ndrange iteration space: global and local sizes based on
+	// parameters obtained from user.
+
+	// Refer to the sample documentation for clarification about
+	// how work is devided among work-groups and work-items.
+	// -----------------------------------------------------------------------
+
+	size_t global_size[1] = {
+		cmdparser.vertex_count.getValue()
+	};
+
+
+	// -----------------------------------------------------------------------
+	// Loop with the kernel invocation
+	// -----------------------------------------------------------------------
+
+	for (int i = 0; i < cmdparser.iterations.getValue(); i++)
+	{
+		// Here we start measuring host time for kernel execution
+		double start = time_stamp();
+
+		cout << "Invoking kernel \n";
+		err = clEnqueueNDRangeKernel(
+			oclobjects.queue,
+			sendMsgKernel.kernel,
+			1,
+			0,
+			global_size,
+			NULL,
+			0, 0, 0
+			);
+
+		//read the output message on edges
+		clEnqueueReadBuffer(
+			oclobjects.queue,
+			edge_msg_buffer,
+			CL_TRUE,
+			0,
+			edge_msg_memory_size,
+			edgeArray.message,
+			0, NULL, NULL
+			);
+
+		cout << "After send message kernel\n";
+		for (int i = 0; i < edgeCount; i++){
+			cout << edgeArray.message[i] << " ";
+		}
+		cout << endl;
+
+		err = clEnqueueNDRangeKernel(
+			oclobjects.queue,
+			combineKernel.kernel,
+			1,
+			0,
+			global_size,
+			NULL,
+			0, 0, 0
+			);
+
+		//read the output ranks on vertices
+		clEnqueueReadBuffer(
+			oclobjects.queue,
+			vertex_rank_output_buffer,
+			CL_TRUE,
+			0,
+			vertex_rank_memory_size,
+			vertexArray.vertex_rank,
+			0, NULL, NULL
+			);
+
+		SAMPLE_CHECK_ERRORS(err);
+
+		err = clFinish(oclobjects.queue);
+		SAMPLE_CHECK_ERRORS(err);
+
+
+		// It is important to measure end host time after clFinish call
+		double end = time_stamp();
+
+		double time = end - start;
+		cout << "Host time: " << time << " sec.\n";
+
+	}
+
+	// deallocated resources
+	clReleaseMemObject(vertex_rank_buffer);
+	clReleaseMemObject(vertex_edge_count_buffer);
+	clReleaseMemObject(edge_msg_buffer);
+	clReleaseMemObject(edge_offset_buffer);
+	clReleaseMemObject(vertex_rank_output_buffer);
+
+	cout << "Vertex rank after Medusa:" << endl;
+	for (int i = 0; i < static_cast<int> (vertex_count); i++) {
+		cout << vertexArray.vertex_rank[i] << " ";
+	}
+
+	breakPoint();
+
+}
+
+void invokeMedusa(CmdParserMedusa cmdparser,
+	int vertex_count,
+	int edgeCount,
+	OpenCLBasic& oclobjects,
+	OpenCLProgramOneKernel& sendMsgKernel,
+	OpenCLProgramOneKernel& combineKernel) {
+	// Call medusa with required type of elements
+	if (cmdparser.arithmetic_int.isSet())
+	{
+		VertexArray<int> vertexArray;
+		EdgeArray<int> edgeArray;
+		constructData(vertex_count, edgeCount, vertexArray, edgeArray);
+		medusa<int>(cmdparser, oclobjects, sendMsgKernel, combineKernel, vertex_count, edgeCount, vertexArray, edgeArray);
+	}
+}
+
+#endif
